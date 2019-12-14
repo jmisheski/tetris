38759d92398d19591c10445dfa2bb47cac12b575
jmisheski <56281339+jmisheski@users.noreply.github.com>
2019-12-13 19:59:44 -0700

    Update main.js

--- tetris/js/main.js
+++ tetris/js/main.js
@@ -1,35 +1,5 @@
 
-function startGame() {
-    var box = document.getElementById("box1");
-    console.log(box)
-    var pos = 0;
-    var t = setInterval(down, 1000);
-    function down() {
-        if (pos >= 550) {
-            clearInterval(t);
-        } else {
-            pos += 50;
-            box.style.top = pos + 'px';
-        };
-    }
-};
-window.addEventListener("click", startGame, {once: true});
-var pos = 200;
-var top = 0
-function moveLeft() {
-    var box = document.getElementById("box1");
-    if (pos < 400 && box.style.top < "550px") {
-        top += 50
-        pos += 50;
-        box.style.right = pos + "px";
-    }
-};
-function moveRight() {
-    var box = document.getElementById("box1");
-    if (pos > 0 && box.style.top < "550px") {
-        pos -= 50;
-        box.style.right = pos + "px";
-    }var svgNS = "http://www.w3.org/2000/svg";
+var svgNS = "http://www.w3.org/2000/svg";
 var count = 0;
 var count2 = 1000000;
 
@@ -140,5 +110,4 @@ function randShape() {
     var shape2 = document.getElementById("shape" + count2);
     console.log(shape2.style.x)
     console.log(shape.style.fill)
-};
 };
